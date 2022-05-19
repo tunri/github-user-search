60d52cb4047fd46d380b30395f9af2eb7a6a78c8
jerson <jersonvictor24@gmail.com>
2022-05-19 14:26:48 -0500

    set fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import 'assets/styles/styles.scss';
+
 import App from "app";
 
 import reportWebVitals from "./reportWebVitals";
